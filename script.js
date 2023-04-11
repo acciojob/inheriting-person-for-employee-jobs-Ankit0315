f0e0361afc3fa0a2bb1886b8b4a205cf78e3aada
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-11 16:32:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+		
+	}
+	greet(){
+console.log(`Hello, my name is ${this._name}, I am ${this._age} year old.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
+		this._name= name;
+		this._age= age;
+		this._jobTitle=jobTitle;
+	}
+	__proto__:Person;
+	jobGreet(){
+console.log(`Hello, my name is ${this._name}, I am ${this._age} year old, and my job title is ${this._jobTitle}.`)
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
