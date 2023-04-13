985a85bafc6895fc960976609276291c68f34f59
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 13:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function Person(name, age) {
+	
 	this.name=name;
 	this.age=age;
 }
 Person.prototype.greet=function(){
-	return "Hello, my name is " + this.name +", I am "+this.age+" years old.";
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -12,8 +13,9 @@ function Employee(name, age, jobTitle) {
 }
 
 Employee.prototype=Object.create(Person.prototype);
+
 Employee.prototype.jobGreet=function(){
-	return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 };
 window.Person = Person;
 window.Employee = Employee;
